d806f7655cd2a88b0010f0ae65e055d713638037
Demar English <demarenglish@Mac.attlocal.net>
2024-12-24 14:46:18 -0500

    Adding Button logic

--- src/index.js
+++ src/index.js
@@ -5,45 +5,115 @@ import "./styles.css";
 
 // The Book Form Should have A Book Title.
 // The Book Form Should have A Book Author.
-// The Book Form Should have A Book Pages. 
+// The Book Form Should have A Book Pages.
 // The Should show if the book has book read or not.
 
 // I should create a factory function to have the need fields.
 
-const myLibrary = [];
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-function Book(isTitle, isAuthor, isPages, isRead) {
-    this.isTitle = isTitle;
-    this.isAuthor = isAuthor;
-    this.isPages = isPages;
-    this.isRead = isRead;
-}
+    getTitle() {
+        return this.title;
+    }
 
-function addBookToLibrary() {
-    let newBook = new Book('New Book', 'J.K', 293, true);
-    myLibrary.push(newBook);
-    validateBook(myLibrary);
-}
+    getAuthor() {
+        return this.author;
+    }
 
-console.log(addBookToLibrary())
+    getPages() {
+        return this.pages;
+    }
 
-function validateBook(getLibrary) {
-    const getBook = [...getLibrary];
-    
-    getBook.forEach((book) => {
-        
-        const currentBook = book;
-        
-        currentBook.isTitle ? validateBookTitle(currentBook.isTitle) : bookTitleError(currentBook.isTitle);
-
-    });
+    getRead() {
+        return this.read;
+    }
 
 }
 
-function validateBookTitle(bookTitle) {
-    console.log(bookTitle);
+class validateBook extends Book {
+    constructor(title, author, pages, read) {
+        super(title, author, pages, read);
+    }
+
+    checkTitle() {
+        if (this.getTitle() !== "") {
+            return this.getTitle();
+        } else {
+            return "Please enter a title";
+        }
+    }
+
+    checkAuthor() {
+        if (this.getAuthor() !== "") {
+            return this.getAuthor();
+        } else {
+            return "Please enter an author";
+        }
+    }
+
+    checkPages() {
+        if (this.getPages() !== "" || this.getPages() > 0) {
+            return this.getPages();
+        } else {
+            return "Please enter a valid number of pages";
+        }
+    }
+
+    checkRead() {
+    }
+
 }
 
-function bookTitleError(bookTitle) {
-    console.log(bookTitle);
+class addBookToLibrary extends validateBook {
+    constructor(title, author, pages, read) {
+        super(title, author, pages, read);
+        this.createBook = new Book(title, author, pages, read);
+        this.myLibrary = [];
+    }
+
+    getBook() {
+        console.log(this.createBook);
+    }
 }
+
+let book = new addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+console.log(book.getBook());
+
+// DOM Logic
+
+class buttonLogic {
+    constructor() {
+        this.cancelButton = document.querySelector("#cancel");
+        this.submitButton = document.querySelector("#submit");
+        this.createBook = document.querySelector("#create-book");
+    }
+
+    getCancelButton() {
+        this.cancelButton.addEventListener("click", () => {
+            alert("Cancel Button");
+        });
+    }
+
+    getSubmitButton() {
+        this.submitButton.addEventListener("click", () => {
+            alert("Submit Button");
+        });
+    }
+
+    getCreateBook() {
+        this.createBook.addEventListener("click", () => {
+            alert("Create Book Button");
+        });
+    }
+}
+
+let button = new buttonLogic();
+button.getCancelButton();
+button.getSubmitButton();
+button.getCreateBook();
